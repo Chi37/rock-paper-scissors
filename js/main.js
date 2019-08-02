5bc31b11228e5d95f3ef3bdce16576f616148fe8
Chi Huynh <chi@skyhawk-277gt.local>
2019-08-02 10:25:57 -0700

    mared up html and added JS variables and render function

--- js/main.js
+++ js/main.js
@@ -3,14 +3,53 @@ const beepAudio = new Audio('http://soundbible.com/mp3/Robot_blip-Marianne_Gagno
 const shootAudio = new Audio('http://soundbible.com/mp3/shooting_star-Mike_Koenig-1132888100.mp3');
 
 /*----- app's state (variables) -----*/
-
+let scores, results, winner;
 
 /*----- cached element references -----*/
 
+//in render, we are grabbing something (element p-score id) over and over again, we should cache
+const scoresEl = {
+    p: document.getElementById('p-score'),
+    t: document.getElementById('t-score'),
+    c: document.getElementById('c-score'),
+}
 
 /*----- event listeners -----*/
-
+document.querySelector('main button')
+.addEventListener('click', playRound);
 
 /*----- functions -----*/
 
-alert('ready!')
+init();
+
+// update state variables with init, then call render
+
+function init() {
+    scores = {
+        p: 0,
+        t: 0,
+        c: 0
+    };
+
+    results = {
+        p: 'r',
+        c: 'r'
+    };
+
+    winner = null;
+    render();
+}
+
+function render() {
+ //render scores
+ for (let score in scores){
+     console.log(score)
+     scoresEl[score].textContent = scores[score];
+ }
+
+}
+
+function playRound() {
+
+}
+// alert('ready!')
